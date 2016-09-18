580c350339ab8a44f092f544b80fe2a117ca8515
Felix <1058240359@qq.com>
2016-09-18 18:56:22 +0800

    Add a login registration effect

---
+++ app/src/main/java/com/felixzr/mdlogin/activity/LoginAct.java
@@ -0,0 +1,142 @@
+package com.felixzr.mdlogin.activity;
+
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.VideoView;
+
+import com.felixzr.mdlogin.R;
+import com.felixzr.mdlogin.View.CatchScrollLayout;
+import com.felixzr.mdlogin.View.SignUpContainer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class LoginAct extends AppCompatActivity {
+
+    public static final String VIDEO_NAME = "welcome_video.mp4";
+    private VideoView mVideoView;
+
+    private CatchScrollLayout mCatchScrollLayout;
+    private SignUpContainer mSignUpContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        checkVersion();
+        setContentView(R.layout.act_login);
+        initView();
+        isExists();
+    }
+
+    private void initView() {
+        mVideoView = (VideoView) findViewById(R.id.videoView);
+        mCatchScrollLayout = (CatchScrollLayout) findViewById(R.id.catch_sroll_layout);
+        mSignUpContainer = (SignUpContainer) findViewById(R.id.sign_up_container);
+
+        mCatchScrollLayout.setIScrollCallBack(new CatchScrollLayout.IScrollCallBack() {
+            @Override
+            public void onScrollProcess(int process, boolean isLeft) {
+                if (!isLeft){
+                    process = 100 - process;
+                }
+                mSignUpContainer.setAnimProportion(process);
+            }
+        });
+
+        mSignUpContainer.setIConfirmCallBack(new SignUpContainer.IConfirmCallBack() {
+            @Override
+            public void goNext() {
+
+            }
+        });
+    }
+
+
+
+    private void checkVersion() {
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+            window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+          *//*  window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);*//*
+        }
+*/
+    }
+
+    private void playVideo(File videoFile) {
+        mVideoView.setVideoPath(videoFile.getPath());
+        mVideoView.setLayoutParams(new RelativeLayout.LayoutParams(-1, -1));
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+                mediaPlayer.start();
+            }
+        });
+    }
+    @NonNull
+    private File copyVideoFile() {
+        File videoFile;
+        try {
+            FileOutputStream fos = openFileOutput(VIDEO_NAME, MODE_PRIVATE);
+            InputStream in = getResources().openRawResource(R.raw.welcome_video);
+            byte[] buff = new byte[1024];
+            int len = 0;
+            while ((len = in.read(buff)) != -1) {
+                fos.write(buff, 0, len);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        videoFile = getFileStreamPath(VIDEO_NAME);
+        if (!videoFile.exists())
+            throw new RuntimeException("video file has problem, are you sure you have welcome_video.mp4 in res/raw folder?");
+        return videoFile;
+    }
+
+    private void isExists() {
+        File videoFile = getFileStreamPath(VIDEO_NAME);
+        if (!videoFile.exists()) {
+            videoFile = copyVideoFile();
+        }
+        playVideo(videoFile);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mVideoView.stopPlayback();
+    }
+}
--- app/src/main/java/com/felixzr/mdlogin/activity/MainAct.java
+++ app/src/main/java/com/felixzr/mdlogin/activity/MainAct.java
@@ -1,7 +1,9 @@
-package com.felixzr.mdlogin;
+package com.felixzr.mdlogin.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.felixzr.mdlogin.R;
 
 public class MainAct extends AppCompatActivity {
 
